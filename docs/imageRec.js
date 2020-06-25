83b7fed66fb5c66adaccc815ed7337abf83aff91
Amy <31504971+Amarii@users.noreply.github.com>
2020-06-26 01:36:37 +0200

    asd

--- docs/imageRec.js
+++ docs/imageRec.js
@@ -27,7 +27,7 @@ async function init() {
     document.getElementById("webcam-container").appendChild(webcam.canvas);
     labelContainer = document.getElementById("label-container");
     for (let i = 0; i < maxPredictions; i++) { // and class labels
-        labelContainer.appendChild(document.createElement("div"));
+        labelContainer.appendChild(document.createElement("img"));
     }
     let index;
 }
@@ -48,7 +48,10 @@ async function predict() {
     for (let i = 0; i < maxPredictions; i++) {
         if (highestChance == prediction[i].probability.toFixed(2)) {
             labelContainer.childNodes[0].innerHTML = prediction[i].className + "!"
+            labelContainer.childNodes[0].alt = prediction[i].className
             msg.text = prediction[i].className
+            labelContainer.childNodes[0].width = "100px"
+
             // if (index == 50) {
             //     //window.speechSynthesis.speak(msg)
             //     index = 0;
